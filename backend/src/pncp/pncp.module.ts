da83afe800f73858acf4a4da4fc4dee2772f0bb4
Hobson <hobsonbmn@gmail.com>
2026-07-29 16:25:34 -0300

    fix: resolve DI error injecting OportunidadeModel in PncpModule

--- backend/src/pncp/pncp.module.ts
+++ backend/src/pncp/pncp.module.ts
@@ -3,9 +3,14 @@ import { HttpModule } from '@nestjs/axios';
 import { PncpClientService } from './services/pncp-client/pncp-client.service';
 import { PncpController } from './controllers/pncp/pncp.controller';
 import { ComprasDadosAbertosService } from './services/compras-dados-abertos/compras-dados-abertos.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Oportunidade, OportunidadeSchema } from '../oportunidade/oportunidade.schema';
 
 @Module({
-  imports: [HttpModule],
+  imports: [
+    HttpModule,
+    MongooseModule.forFeature([{ name: Oportunidade.name, schema: OportunidadeSchema }])
+  ],
   providers: [PncpClientService, ComprasDadosAbertosService],
   controllers: [PncpController],
   exports: [PncpClientService, ComprasDadosAbertosService],
