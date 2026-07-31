aa5ae0bbd6569ee2cc4808da630b41ec56d37038
Hobson <hobsonbmn@gmail.com>
2026-07-31 03:43:08 -0300

    fix: Corrigir CrashLoopBackOff injetando dependencias faltantes no OportunidadeModule

--- backend/src/oportunidade/oportunidade.module.ts
+++ backend/src/oportunidade/oportunidade.module.ts
@@ -3,16 +3,22 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { OportunidadeService } from './oportunidade.service';
 import { OportunidadeController } from './oportunidade.controller';
 import { Oportunidade, OportunidadeSchema } from './oportunidade.schema';
+import { SimulacaoEstrategia, SimulacaoSchema } from './schemas/simulacao.schema';
 import { PncpModule } from '../pncp/pncp.module';
 import { ProdutoModule } from '../produto/produto.module';
+import { FinanceiroModule } from '../financeiro/financeiro.module';
+import { Cotacao, CotacaoSchema } from '../cotacao/cotacao.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Oportunidade.name, schema: OportunidadeSchema },
+      { name: SimulacaoEstrategia.name, schema: SimulacaoSchema },
+      { name: Cotacao.name, schema: CotacaoSchema },
     ]),
     PncpModule,
     forwardRef(() => ProdutoModule),
+    forwardRef(() => FinanceiroModule),
   ],
   controllers: [OportunidadeController],
   providers: [OportunidadeService],
